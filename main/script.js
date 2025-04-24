1485fe8f05e6e1c47cd88dd5a8bef3e93b850fdd
DJU227 <panseradjulio@gmail.com>
2025-04-23 21:07:00 -0300

    parte do e-service

---
+++ main/script.js
@@ -0,0 +1,5 @@
+ const myCarouselElement = document.querySelector('#carouselExampleDark');
+  const carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: 4000, // 4 segundos
+    ride: 'carousel'
+  });
